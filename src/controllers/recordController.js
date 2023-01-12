997832eb4f8c01fa71d4efe69251d24bbea803cd
Heyder Belalcazar <heyder.belalcazar@davinci.tech>
2023-01-12 14:32:53 -0500

    agregando registros a la bd

---
+++ src/controllers/recordController.js
@@ -0,0 +1,17 @@
+const recordService = require('../services/recordService')
+
+const getRecordForWorkout = (req, res) => {
+    try {
+      const { workoutId } = req.params;   
+      const record = recordService.getRecordForWorkout(workoutId);
+      res.send({ status: "OK", data: record });
+    } catch (error) {
+      res
+        .status(error?.status || 500)
+        .send({ status: "FAILED", data: { error: error?.message || error } });
+    }
+  };
+
+module.exports = {
+  getRecordForWorkout
+}
